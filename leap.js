c530ae1edf218106a8cbe1c83e1f2dcbd68c5da2
Stephan Vock <stephan.vock@innogames.de>
2013-09-13 12:23:44 +0200

    added stop command

--- leap.js
+++ leap.js
@@ -16,31 +16,41 @@ var leap = function(emitter) {
 
 controller.on("frame", function(frame) {
 	if (!inflight) {
-		//return;
+		return;
 	}
 
+	var event = false;
 	if (frame.hands.length > 0) {
 		var up_down_speed = frame.hands[0].palmVelocity[1];
 		if (up_down_speed > 20) {
 			eventemitter.emit('up', up_down_speed);
+			event = true;
 		} else if (up_down_speed < -20) {
 			eventemitter.emit('down', -1 * up_down_speed);
+			event = true;
 		}
 
 		var right_left_speed = frame.hands[0].palmNormal[0];
 		if (right_left_speed > 0.15) {
 			eventemitter.emit('left', right_left_speed);
+			event = true;
 		} else if (right_left_speed < -0.15) {
 			eventemitter.emit('right', -1 * right_left_speed);
+			event = true;
 		}
 
 		var forward_backward_speed = frame.hands[0].palmNormal[2];
 		if (forward_backward_speed > 0.15) {
 			eventemitter.emit('forward', forward_backward_speed);
+			event = true;
 		} else if (forward_backward_speed < -0.15) {
 			eventemitter.emit('backward', -1 * forward_backward_speed);
+			event = true;
 		}
 
+		if (!event) {
+			eventemitter.emit('stop');
+		}
 	}
 });
 
